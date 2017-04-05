d81ee7690345638740405a02afe1063ee853db37
Terry Torres <terrytorres@gmail.com>
2017-04-05 10:01:51 -0400

    Prior to refactoring to more decoupled delegate model

--- ToDo/AppDelegate.swift
+++ ToDo/AppDelegate.swift
@@ -28,6 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        self.saveContext()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -76,6 +77,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Core Data Saving support
 
     func saveContext () {
+        print("saving main context")
         let context = persistentContainer.viewContext
         if context.hasChanges {
             do {
